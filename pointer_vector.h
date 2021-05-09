e435f5660b42350107924309719d5ec3989b0031
Dehim1 <dehim@live.nl>
2021-05-09 12:36:11 +0200

    Variadic arguments for push_back and resize
    Variadic arguments for push_back and resize to support any custom constructor.
    Also modified initialize_this(size_t size) to preincrement size to prevent 0 size array allocation.

--- pointer_vector.h
+++ pointer_vector.h
@@ -44,22 +44,24 @@ public:
 			iter->~T();
 	}
 
-	void resize(size_t size, const T& data)
+	template<class... _Args>
+	void resize(size_t size, _Args&&... args)
 	{
 		reserve(size);
 		auto iter = end();
 		*reinterpret_cast<uint8_t**>(data_ + sizeof(uint8_t*)) = (data_ + 2 * sizeof(uint8_t*) + size * sizeof(T));
 		for (; iter < end(); ++iter)
-			new (iter) T(data);
+			new (iter) T(args...);
 		for (--iter; iter >= end(); --iter)
 			iter->~T();
 	}
 
-	void push_back(const T& data)
+	template<class... _Args>
+	void push_back(_Args&&... args)
 	{
 		if (end() == *reinterpret_cast<T**>(data_))
 			reallocate_copy_destroy(2 * size_t(*reinterpret_cast<uint8_t**>(data_) - data_ - sizeof(uint8_t*)));
-		new (*reinterpret_cast<T**>(data_ + sizeof(uint8_t*))) T(data);
+		new (*reinterpret_cast<T**>(data_ + sizeof(uint8_t*))) T(args...);
 		++*reinterpret_cast<T**>(data_ + sizeof(uint8_t*));
 	}
 
@@ -78,7 +80,7 @@ public:
 
 	size_t size() const
 	{
-		return (*reinterpret_cast<uint8_t**>(data_ + sizeof(uint8_t*)) - data_ - 2 * sizeof(uint8_t*))/sizeof(T);
+		return end() - begin();
 	}
 
 	bool empty() const
@@ -106,7 +108,26 @@ public:
 		return *(begin() + idx);
 	}
 
-	pointer_vector() = default;
+	pointer_vector() {}
+	~pointer_vector() {}
+
+	template<class T2>
+	pointer_vector(const pointer_vector<T2>& rhs)
+	{
+		this->initialize_this(rhs.size());
+		auto& to_iter = *reinterpret_cast<T**>(data_ + sizeof(uint8_t*));
+		for (auto& ref : rhs)
+			new (to_iter++) T(ref);
+	}
+
+	template<class T2>
+	pointer_vector(const pointer_vector<T2>& rhs, float growth_factor)
+	{
+		this->initialize_this(size_t(std::ceil(rhs.size()*growth_factor)));
+		auto& to_iter = *reinterpret_cast<T**>(data_ + sizeof(uint8_t*));
+		for (auto& ref : rhs)
+			new (to_iter++) T(ref);
+	}
 
 	pointer_vector(void* pv)
 	{
@@ -122,6 +143,13 @@ public:
 	{
 		data_ = new uint8_t[2 * sizeof(uint8_t*) + sizeof(T)];
 		*reinterpret_cast<uint8_t**>(data_) = data_ + 2 * sizeof(uint8_t*) + sizeof(T); //End of allocated storage pointer
+		*reinterpret_cast<uint8_t**>(data_ + sizeof(uint8_t*)) = data_ + 2 * sizeof(uint8_t*); //End of used storage pointer
+	}
+
+	void initialize_this(size_t size)
+	{
+		data_ = new uint8_t[2 * sizeof(uint8_t*) + (++size) * sizeof(T)];
+		*reinterpret_cast<uint8_t**>(data_) = data_ + 2 * sizeof(uint8_t*) + size * sizeof(T); //End of allocated storage pointer
 		*reinterpret_cast<uint8_t**>(data_ + sizeof(uint8_t*)) = data_ + 2 * sizeof(uint8_t*); //End of used storage pointer
 	}
 
@@ -149,7 +177,7 @@ public:
 		return *(end() - 1);
 	}
 
-	const T& operator[](size_t idx) const 
+	const T& operator[](size_t idx) const
 	{
 		return *(begin() + idx);
 	}
